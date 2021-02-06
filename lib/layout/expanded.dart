38884ad95f4b92d6a7da19dbf28b9cc9deeac518
manoj1995madushanka <madushankamanoj414@gmail.com>
2021-02-06 19:21:26 +0530

    expanded

---
+++ lib/layout/expanded.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("hello"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: [
+              new Text("Image example"),
+              // if expanded did not added exception will trow for small screen size
+              new Expanded(child: new Image.asset('images/test.jpg'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
